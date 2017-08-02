c6c6ce2dcb02b15ceae44ab3ade2756c3fff2895
Ziamak Ezzat <ziamak.ezzat@protonmail.com>
2017-08-02 23:36:51 +0200

    Picture

--- server/server.py
+++ server/server.py
@@ -11,13 +11,14 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
  
         # Send headers
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type','image/jpeg')
         self.end_headers()
  
         print('path: ',self.path)
         # Send message back to client
-        message = open('./../res/html/index.html','r')
-        # Write content as utf-8 data
+        message = open('./../res/html/fountain.jpg','r')
+
+        # Write content as  utf-8 data
         self.wfile.write(bytes(message.read(), "utf8"))
         return
  
@@ -25,7 +26,7 @@ def run():
   print('starting server...')
  
   # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  # Choose port 8080, for port 80, which is nor mally used for a http server, you need root access
   server_address = ('127.0.0.1', 8081)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
