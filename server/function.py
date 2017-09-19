a2c97df8f6dcb39e33334612955e78e5710cae88
Ziamak Ezzat <ziamak.ezzat@protonmail.com>
2017-09-20 01:04:13 +0200

    bilder 2.0

---
+++ server/function.py
@@ -0,0 +1,10 @@
+path = './../res/pictures/hello.jpg'
+
+def getFileFromPath(x):
+	x = x[0:5]
+	return x
+
+
+
+name = getFileFromPath(path)
+print(name)
--- server/server.py
+++ server/server.py
@@ -4,16 +4,47 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
  
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
- 
+
+
+  def getFileFromPath(a):
+    if a == '/picture/Cat.jpg':
+     # b = print.a.find(".jpg")
+      a = a[8:-4]
+      print(a)
+    # self.getImage('./../res'+ a )
+    # if self.path == '*'.jpg:
+
+    print ("TEEEEEEEEEEEEEEEEEEEEEEEEEEEST")
+    print (self.getImage('./../res/'+a))
+
+    return a
+  
   # GET
   def do_GET(self):
         print('path: ',self.path)
         if self.path == '/':
           self.getIndex()
-        elif self.path == '/picture/fountain.jpg':
-          self.getImage()
+          getFileFromPath(a) 
+          #Bilder müssen über die Funktion aufgerufen werden
         elif self.path == '/css/mystyle.css':
-          self.getCss()
+          self.getCss()  
+        # self.path == .jpg
+        #     '/picture/Cat.jpg' --> './../res/picture/Cat.jpg'
+        #     '/picture/dogs.jpeg' --> './../res/picture/dogs.jpeg'
+        # ...
+        #     '/picture/***.jpg' --> './../res/picture/***.jpg'
+        """
+        elif self.path == '/picture/Cat.jpg':
+          self.getImage('./../res/picture/Cat.jpg')
+        elif self.path == '/picture/dogs.jpeg':
+          self.getImage('./../res/picture/dogs.jpeg')
+        elif self.path == '/picture/fountain.jpg':
+          self.getImage('./../res/picture/fountain.jpg')
+        elif self.path == '/picture/tiger.jpg':
+          self.getImage('./../res/picture/tiger.jpg')
+          """
+        
+        
         return
 
 
@@ -45,16 +76,28 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(message.read(), "utf-16"))
         return
  
-  def getImage(self):
+  def getImage(self, mypath):
         self.send_response(200)
  
         # Send headers
         self.send_header('Content-type','image/jpeg')
         self.end_headers()
+        print('mypath: ' + mypath)
  
+        message = open(mypath, 'rb')
         # Send message back to client
-        message = open('./../res/picture/fountain.jpg','rb')
-
+        """
+        if self.path == '/picture/fountain.jpg':
+          message = open('./../res/picture/fountain.jpg','rb')
+        elif self.path == '/picture/Cat.jpg':
+          message = open('./../res/picture/Cat.jpg','rb')
+        elif self.path == '/picture/dogs.jpeg':
+          message = open('./../res/picture/dogs.jpeg','rb')
+        elif self.path == '/picture/tiger.jpg':
+          message = open('./../res/picture/tiger.jpg','rb')
+        """
+        
+     #   message = open('./../res/picture/fountain.jpg','rb')
         # Write content as  utf-8 data
         self.wfile.write(bytearray(message.read()))
         return
